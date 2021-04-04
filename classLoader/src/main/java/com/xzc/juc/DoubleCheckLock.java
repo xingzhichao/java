80df100932703a7bea6f640d8578a76bcd4962bb
chenshuchuan <chenshuchuan@ndmooc.com>
2021-04-04 17:42:11 +0800

    双重锁提交

---
+++ classLoader/src/main/java/com/xzc/juc/DoubleCheckLock.java
@@ -0,0 +1,67 @@
+package com.xzc.juc;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class DoubleCheckLock {
+    private volatile static DoubleCheckLock instance = null;
+//    private static DoubleCheckLock instance = null;
+
+    /**
+     * DCL 双重锁
+     *
+     * @return
+     */
+    public static DoubleCheckLock getInstance() {
+        //第一次检测
+        if (null == instance) {
+            //同步
+            synchronized (DoubleCheckLock.class) {
+                if (null == instance) {
+                    //多线程环境下可能会出现问题的地方
+                    //todo : 模拟并发请求，并没有验证得到想要的结果
+                    instance = new DoubleCheckLock();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+//        ExecutorService executorService = Executors.newFixedThreadPool(100);
+//        Runnable runnable = new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    Thread.sleep(100);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                DoubleCheckLock instance1 = getInstance();
+//                System.out.println(instance1);
+//            }
+//        };
+//
+//            executorService.execute(runnable);
+//            executorService.execute(runnable);
+//            executorService.execute(runnable);
+//            executorService.execute(runnable);
+//            executorService.execute(runnable);
+//            executorService.execute(runnable);
+//            executorService.execute(runnable);
+
+//        executorService.shutdown();
+
+
+        for (int i = 0; i < 100; i++) {
+            final int num = i;
+            new Thread(new Runnable() {
+                public void run() {
+                    DoubleCheckLock instance = getInstance();
+                    System.out.println(instance);
+                }
+            }).start();
+        }
+
+    }
+}
